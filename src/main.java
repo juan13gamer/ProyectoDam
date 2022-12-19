bf7a1b920976038781eafafcd798ad040890566d
DiavoloCalvo <j.avila@insjoanbrudieu.cat>
2022-12-19 16:56:15 +0100

    Cambio del valor i

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int i = 0;
+		int i = 1;
 		if (i % 2 == 0) {
 			System.out.println("i: " + i);
 		}
